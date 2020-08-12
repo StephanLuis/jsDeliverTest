f25b630d383a9687c4f7724b1dfd8d5f8aba0fde
StephanLuis <stephan@learnsense.org>
2020-08-12 16:21:45 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -7,42 +7,55 @@
 //\_/\_/(____/(____/  (__)  \_/\_/ \___)(__\_)\_/\_/ \___/(____)(____/
 
 
+(function () {
+    // your page initialization code here
+    // the DOM will be available here
 
-(function ($) {
+    var jquiLink = document.createElement("link");
+    jquiLink.rel = "stylesheet";
+    jquiLink.href = "https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/jquery-ui.min.css";
+    document.head.appendChild(jquiLink);
 
 
-    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/jquery-ui.min.css">');
-    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/timeslider.min.css">')
-    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.css">')
+
+
+
+})();
+//(function ($) {
+
+
+//    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/jquery-ui.min.css">');
+//    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/timeslider.min.css">')
+//    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.css">')
 
 
        
 
-    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery.min.js"></script>')
-    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery-ui.min.js"></script>')
-    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.js"></script>')
+//    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery.min.js"></script>')
+//    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery-ui.min.js"></script>')
+//    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.js"></script>')
 
-    // check if jQuery is present, if not add it
+//    // check if jQuery is present, if not add it
 
-    if (typeof jQuery === 'undefined') {
-        // jQuery is NOT available, add script
-
-
-    }
-
-    // check if jQueryUI is present, if not add it
-
-    if (typeof (jQuery.ui) == "object") { if (typeof (jQuery.ui.tabs) == "function") { console.log("jQuery-UI loaded") } }
-    // if not then add script
+//    if (typeof jQuery === 'undefined') {
+//        // jQuery is NOT available, add script
 
 
-// add page.css using JS like done here https://stackoverflow.com/a/51183077/2455159
+//    }
+
+//    // check if jQueryUI is present, if not add it
+
+//    if (typeof (jQuery.ui) == "object") { if (typeof (jQuery.ui.tabs) == "function") { console.log("jQuery-UI loaded") } }
+//    // if not then add script
 
 
-// add page.js
+//// add page.css using JS like done here https://stackoverflow.com/a/51183077/2455159
+
+
+//// add page.js
 
 
 
 
-})(jQuery);
+//})(jQuery);
 
