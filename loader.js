a24c58c52e4818a1d9063444b0cd3cf5c2ccc268
StephanLuis <stephan@learnsense.org>
2020-08-12 16:10:52 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,3 +1,5 @@
+
+
  
 //  __   ____  ____    ____   __    ___  __ _   __    ___  ____  ____
 // / _\ (    \(    \  (  _ \ / _\  / __)(  / ) / _\  / __)(  __)/ ___)
@@ -5,17 +7,32 @@
 //\_/\_/(____/(____/  (__)  \_/\_/ \___)(__\_)\_/\_/ \___/(____)(____/
 
 
-// check if jQuery is present, if not add it
 
-if (typeof jQuery === 'undefined') {
-    // jQuery is NOT available, add script
+(function ($) {
 
 
-}
+    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/jquery-ui.min.css">');
+    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/css/timeslider.min.css">')
+    $('head').insertBefore('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.css">')
 
-// check if jQueryUI is present, if not add it
 
-if (typeof (jQuery.ui) == "object") { if (typeof (jQuery.ui.tabs) == "function") { console.log("jQuery-UI loaded") } }
+       
+
+    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery.min.js"></script>')
+    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/External/js/jquery-ui.min.js"></script>')
+    $('body').insertAfter('<script src="https://cdn.jsdelivr.net/gh/StephanLuis/jsDeliverTest@t1/page.min.js"></script>')
+
+    // check if jQuery is present, if not add it
+
+    if (typeof jQuery === 'undefined') {
+        // jQuery is NOT available, add script
+
+
+    }
+
+    // check if jQueryUI is present, if not add it
+
+    if (typeof (jQuery.ui) == "object") { if (typeof (jQuery.ui.tabs) == "function") { console.log("jQuery-UI loaded") } }
     // if not then add script
 
 
@@ -23,3 +40,9 @@ if (typeof (jQuery.ui) == "object") { if (typeof (jQuery.ui.tabs) == "function")
 
 
 // add page.js
+
+
+
+
+})(jQuery);
+
